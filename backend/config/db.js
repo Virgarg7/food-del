11f442e23940f5dec630d26716a93c5d190b9647
Vir garg <vir.garg.in@gmail.com>
2024-06-13 17:01:08 +0530

    Adding Frontend , Backend and Admin panel

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const  connectDB = async () =>{
+    await mongoose.connect('mongodb+srv://virgarg772003:jptnqsBaGbAVplQp@cluster0.nzkw77d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/food-del').then(()=>console.log("DB Connected"))
+}
+
+
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error.
